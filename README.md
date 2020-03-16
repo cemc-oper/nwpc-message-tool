f80485bb803b74c527931ca9d4d06534f1dda789
perillaroc <perillaroc@gmail.com>
2020-03-16 14:23:59 +0800

    :tada: initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# nwpc-message-tool
+
+Tools for nwpc-message-client.
+
+## Getting started
+
+## Config
+
+## LICENSE
+
+Copyright &copy; 2020 perillaroc at nwpc-oper.
+
+`nwpc-message-tool` is license under [GPL v3.0](./LICENSE.md).
---
+++ setup.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+import codecs
+from os import path
+import io
+import re
+
+with io.open("nwpc_message_tool/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+here = path.abspath(path.dirname(__file__))
+
+with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nwpc-message-tool',
+
+    version=version,
+
+    description='Tool for nwpc-message-client.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/nwpc-oper/nwpc-message-tool',
+
+    author='perillaroc',
+    author_email='perillaroc@gmail.com',
+
+    license='GPLv3',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+
+    keywords='nwpc message tool',
+
+    packages=find_packages(exclude=['docs', 'tests']),
+
+    install_requires=[
+        "pyyaml",
+        "loguru",
+        "click",
+        "pandas",
+        "scipy",
+        "pyprind",
+    ],
+
+    extras_require={
+        'test': ['pytest'],
+        'cov': ['pytest-cov', 'codecov']
+    },
+
+    entry_points={
+        "console_scripts": [
+        ],
+    }
+)
