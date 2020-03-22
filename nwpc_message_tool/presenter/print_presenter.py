8acb553bfa5e5325d1d56f23d516b0a9d233dcec
perillaroc <perillaroc@gmail.com>
2020-03-22 11:21:23 +0800

    :art: add all properties into dataframe columns

--- nwpc_message_tool/presenter/print_presenter.py
+++ nwpc_message_tool/presenter/print_presenter.py
@@ -10,7 +10,7 @@ class PrintPresenter(Presenter):
         super(PrintPresenter, self).__init__()
 
     def show(self, df: pd.DataFrame):
-        df["start_time"] = df.start_time.apply(lambda x: x.strftime("%Y%m%d%H"))
+        df.start_time = df.start_time.apply(lambda x: x.strftime("%Y%m%d%H"))
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(df)
         print(f"Latest time: {df.time.max()}")
--- nwpc_message_tool/processor/table_processor.py
+++ nwpc_message_tool/processor/table_processor.py
@@ -8,27 +8,47 @@ from nwpc_message_tool.message import ProductionEventMessage
 
 
 class TableProcessor(object):
-    def __init__(self):
-        pass
+    def __init__(self, columns: typing.List[str] or None = None):
+        self.columns = [
+            "system",
+            "stream",
+            "type",
+            "name",
+            "start_time",
+            "forecast_hour",
+            "time",
+            "event",
+            "status",
+        ]
+        if columns is not None:
+            self.columns = columns
 
     def process_messages(self, messages: typing.Iterable[ProductionEventMessage]) -> pd.DataFrame:
-        df = pd.DataFrame(columns=["start_time", "forecast_hour", "time"])
+        df = pd.DataFrame(columns=self.columns)
         for result in messages:
             hours = get_hour(result)
             message_time = result.time.ceil("S")
             current_df = pd.DataFrame(
                 {
+                    "system": [result.system],
+                    "stream": [result.stream],
+                    "type": [result.production_type],
+                    "name": [result.production_name],
                     "start_time": [pd.to_datetime(result.start_time)],
                     "forecast_hour": [hours],
-                    "time": [message_time]
+                    "time": [message_time],
+                    "event": [result.event],
+                    "status": [result.status.name],
                 },
-                columns=["start_time", "forecast_hour", "time"],
+                columns=self.columns,
                 index=[f"{result.start_time.strftime('%Y%m%d%H')}+{hours:03}"]
             )
             df = df.append(current_df)
+
         df["time"] = pd.to_datetime(df["time"], utc=True)
         df["forecast_hour"] = pd.to_numeric(df["forecast_hour"])
         df["start_time"] = pd.to_datetime(df["start_time"], utc=True)
+
         logger.info(f"searching...done")
 
         logger.info(f"get {len(df)} results")
