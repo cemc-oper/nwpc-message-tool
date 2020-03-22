74a24b8b5689eb1c92ccc2c6f3c62e6045f3e968
perillaroc <perillaroc@gmail.com>
2020-03-22 16:57:41 +0800

    :sparkles: add cli for plot.

--- nwpc_message_tool/__main__.py
+++ nwpc_message_tool/__main__.py
@@ -1,5 +1,6 @@
 import click
 from nwpc_message_tool.cli.table import table_cli
+from nwpc_message_tool.cli.plot import plot_cli
 
 
 @click.group()
@@ -9,4 +10,5 @@ def cli():
 
 if __name__ == "__main__":
     cli.add_command(table_cli, name="table")
+    cli.add_command(plot_cli, name="plot")
     cli()
---
+++ nwpc_message_tool/cli/plot.py
@@ -0,0 +1,89 @@
+import click
+from loguru import logger
+
+from nwpc_message_tool._util import get_engine
+from nwpc_message_tool.cli._util import parse_start_time
+from nwpc_message_tool.storage import EsMessageStorage
+from nwpc_message_tool.presenter import (
+    PlotPresenter,
+)
+from nwpc_message_tool.processor import TableProcessor
+
+
+@click.command("plot")
+@click.option("--elastic-server", required=True, multiple=True, help="ElasticSearch servers")
+@click.option("--system", required=True, help="system, such as grapes_gfs_gmf, grapes_meso_3km and so on.")
+@click.option("--production-stream", default="oper", help="production stream, such as oper.")
+@click.option("--production-type", default="grib2", help="production type, such as grib.")
+@click.option("--production-name", default="orig", help="production name, such as orig.")
+@click.option(
+    "--start-time",
+    required=True,
+    metavar="YYYYMMDDHH[/YYYYMMDDHH]",
+    help="start time, one date or a data range.")
+@click.option(
+    "--engine",
+    default="nwpc_message",
+    type=click.Choice(["nwpc_message", "nmc_monitor"]),
+    help="data source"
+)
+@click.option(
+    "--output-type",
+    default="file",
+    type=click.Choice(["file"]),
+    help="output type"
+)
+@click.option(
+    "--output-file",
+    help="output file path",
+)
+def plot_cli(
+        elastic_server,
+        system,
+        production_stream,
+        production_type,
+        production_name,
+        start_time: str,
+        engine,
+        output_type,
+        output_file,
+):
+    """
+    Plot forecast time points.
+    """
+    start_time = parse_start_time(start_time)
+
+    engine = get_engine(engine)
+    logger.info(f"using search engine: {engine.__name__}")
+
+    system = engine.fix_system_name(system)
+    logger.info(f"fix system name to: {system}")
+
+    logger.info(f"searching...")
+    client = EsMessageStorage(
+        hosts=elastic_server,
+        engine=engine,
+    )
+    results = client.get_production_messages(
+        system=system,
+        production_stream=production_stream,
+        production_type=production_type,
+        production_name=production_name,
+        start_time=start_time
+    )
+
+    processor = TableProcessor()
+    table = processor.process_messages(results)
+
+    print(table)
+
+    presenter = PlotPresenter(
+        system=system,
+        output_type=("file",),
+        output_path=output_file,
+    )
+    presenter.show(table)
+
+
+if __name__ == "__main__":
+    plot_cli()
