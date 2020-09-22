2d5307c726249e114143bc894a2b12f67787f38f
perillaroc <perillaroc@gmail.com>
2020-09-22 08:58:20 +0800

    :alembic: add a html page for bokeh

--- nwpc_message_tool/server/main.py
+++ nwpc_message_tool/server/main.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, request
+
+import pandas as pd
+
 
 main_app = Blueprint('main_app', __name__, template_folder='template')
 
@@ -8,3 +11,19 @@ def get_index_page():
     return jsonify({
         'status': 'ok'
     })
+
+
+@main_app.route('/plot/cycle/time-line')
+def get_cycle_time_line_plot():
+    from nwpc_message_tool.server.plot import get_cycle_time_line
+
+    system = request.args.get("system", None)
+    start_time = request.args.get("start-time", None)
+    start_time = pd.to_datetime(start_time, format="%Y%m%d%H")
+    print(start_time)
+
+    output_html = get_cycle_time_line(
+        system=system,
+        start_time=start_time
+    )
+    return make_response(output_html)
