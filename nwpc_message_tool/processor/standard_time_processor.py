c42eb7cb2ddee8d7802b5d3104903d3f2a75f6c3
perillaroc <perillaroc@gmail.com>
2020-12-09 10:59:34 +0800

    :recycle: extract small functions.

--- nwpc_message_tool/processor/standard_time_processor.py
+++ nwpc_message_tool/processor/standard_time_processor.py
@@ -13,6 +13,22 @@ class StandardTimeProcessor(object):
             bootstrap_sample: int = 10,
             quantile: float = 0.99,
     ):
+        """
+
+        Parameters
+        ----------
+        start_hours: typing.List
+            start hour of each cycle with forecast hour list
+            [
+                {
+                    "start_hours": "00",
+                    "forecast_hours": [0, 1, 2, 3, ... ]
+                }
+            ]
+        bootstrap_count: int
+        bootstrap_sample: int
+        quantile: float
+        """
         self.start_hours = start_hours
         self.bootstrap_count = bootstrap_count
         self.bootstrap_sample = bootstrap_sample
@@ -36,30 +52,21 @@ class StandardTimeProcessor(object):
             for hour_index in tqdm(range(len(forecast_hours)), desc=f"{start_hour} hour loop"):
                 forecast_hour = forecast_hours[hour_index]
                 clock_df_hour = df_start_hour[df_start_hour["forecast_hour"] == forecast_hour]
-                means = []
-                for i in range(self.bootstrap_count):
-                    sampled_data = clock_df_hour["clock"].sample(
-                        n=self.bootstrap_sample,
-                        replace=True,
-                    )
-                    means.append(sampled_data.mean())
 
-                bdf = pd.DataFrame(means).applymap(lambda x: x.ceil("s"))
-                upper_bound = bdf.quantile(
-                    self.quantile + (1 - self.quantile) / 2,
-                    numeric_only=False,
-                    interpolation="nearest"
+                means = self._get_means(
+                    clock_df_hour,
+                    bootstrap_sample=self.bootstrap_sample,
+                    bootstrap_count=self.bootstrap_count,
                 )
-                lower_bound = bdf.quantile(
-                    (1 - self.quantile) / 2,
-                    numeric_only=False,
-                    interpolation="nearest",
+
+                standard_time = self._get_bound(
+                    means,
+                    forecast_hour=forecast_hour,
+                    quantile=self.quantile,
                 )
-                standard_times.append({
-                    "forecast_hour": forecast_hour,
-                    "upper_bound": upper_bound[0],
-                    "lower_bound": lower_bound[0],
-                })
+
+                standard_times.append(standard_time)
+
             times.append({
                 "start_hour": start_hour,
                 "times": standard_times,
@@ -81,3 +88,49 @@ class StandardTimeProcessor(object):
         ]
 
         return production_times
+
+    def _get_mean(
+            self,
+            clock_df: pd.DataFrame,
+            bootstrap_sample: int,
+    ) -> pd.Timedelta:
+        sampled_data = clock_df["clock"].sample(
+            n=bootstrap_sample,
+            replace=True,
+        )
+        return sampled_data.mean()
+
+    def _get_means(
+            self,
+            clock_df,
+            bootstrap_sample,
+            bootstrap_count
+    ) -> typing.List[pd.Timedelta]:
+        means = []
+        for i in range(bootstrap_count):
+            mean = self._get_mean(clock_df, bootstrap_sample)
+            means.append(mean)
+        return means
+
+    def _get_bound(
+            self,
+            means: typing.List[pd.Timedelta],
+            forecast_hour: int,
+            quantile: float,
+    ) -> typing.Dict:
+        bdf = pd.DataFrame(means).applymap(lambda x: x.ceil("s"))
+        upper_bound = bdf.quantile(
+            quantile + (1 - quantile) / 2,
+            numeric_only=False,
+            interpolation="nearest"
+        )
+        lower_bound = bdf.quantile(
+            (1 - quantile) / 2,
+            numeric_only=False,
+            interpolation="nearest",
+        )
+        return {
+            "forecast_hour": forecast_hour,
+            "upper_bound": upper_bound[0],
+            "lower_bound": lower_bound[0],
+        }
